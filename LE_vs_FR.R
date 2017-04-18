aebe89d50672b9e1e877db14119e41c7268e9ced
Lawrence Barrett <lawrencebarrett@Lawrences-MacBook-Pro.local>
2017-04-17 21:17:32 -0700

    Initial commit

---
+++ LE_vs_FR.R
@@ -0,0 +1,74 @@
+rm(list = ls())
+cat('\014')
+
+library(tidyr)
+library(dplyr)
+
+# setwd("/Users/lawrencebarrett/Documents/School/msan622/Homework/Homework2/")
+
+life_expectancy <- read.csv("Data/API_SP.DYN.LE00.IN_DS2_en_csv_v2.csv",
+                           header = TRUE, stringsAsFactors = FALSE, skip = 4, check.names = FALSE)
+fertility_rate <- read.csv("Data/API_SP.DYN.TFRT.IN_DS2_en_csv_v2.csv",
+                            header = TRUE, stringsAsFactors = FALSE, skip = 4, check.names = FALSE)
+population <- read.csv("Data/API_SP.POP.TOTL_DS2_en_csv_v2.csv",
+                       header = TRUE, stringsAsFactors = FALSE, check.names = FALSE)
+
+metadata <- read.csv("Data/Metadata_Country_API_SP.DYN.LE00.IN_DS2_en_csv_v2.csv",
+                       header = TRUE, stringsAsFactors = FALSE, check.names = FALSE)
+
+metadata <- metadata[, -6] %>% filter(Region != "")
+
+fertility_rate <- fertility_rate[-c(which(apply(fertility_rate[, -c(3, 4, 60, 61, 62)], MARGIN = 1, function(x) all(is.na(x[3:57])))), 211),
+                                 -c(3, 4, 60, 61, 62)]
+life_expectancy <- life_expectancy[-c(which(apply(life_expectancy[, -c(3, 4, 60, 61, 62)], MARGIN = 1, function(x) all(is.na(x[3:57])))), 211),
+                                 -c(3, 4, 60, 61, 62)]
+population <- population[-c(which(apply(population[, -c(3, 4, 60, 61, 62)], MARGIN = 1, function(x) all(is.na(x[3:57])))), 211),
+                                 -c(3, 4, 60, 61, 62)]
+
+# between_years <- seq(1960, 2014, 0.25)
+# new_cols <- between_years[!(between_years%%1 == 0)]
+# for (i in seq(58, length(new_cols)+57, 1)){
+#   fertility_rate[,i] <- NA
+#   life_expectancy[,i] <- NA
+#   population[,i] <- NA
+# }
+# colnames(fertility_rate) <- c(colnames(fertility_rate)[1:57], new_cols)
+# colnames(life_expectancy) <- c(colnames(life_expectancy)[1:57], new_cols)
+# colnames(population) <- c(colnames(population)[1:57], new_cols)
+
+
+fertility_rate <- gather(fertility_rate, key = "year", value = "fertility_rate", 3:57)
+life_expectancy <- gather(life_expectancy, key = "year", value = "life_expectancy", 3:57)
+population <-gather(population, key = "year", value = "population", 3:57)
+
+country_data <- inner_join(inner_join(fertility_rate, life_expectancy, by = c("Country Name", "Country Code", "year")),
+                          population, by = c("Country Name", "Country Code", "year"))
+
+na_count <- country_data %>% 
+  group_by(`Country Name`) %>% 
+  arrange(year) %>% 
+  summarise(fr_non_na = sum(!is.na(fertility_rate)),
+            le_non_na = sum(!is.na(life_expectancy)),
+            pop_non_na = sum(!is.na(population)))
+
+linear_interpolation <- function(data){
+  missindx <- is.na(data)
+  n <- length(data)
+  allindx <- 1:n
+  indx <- allindx[!missindx]
+  data.vec <- as.vector(data)
+  return(approx(indx, data.vec[indx], 1:n, rule = 2)$y)
+}
+
+
+country_data <- country_data %>% 
+  group_by(`Country Name`) %>% 
+  arrange(year) %>% 
+  mutate(fr_interp = linear_interpolation(fertility_rate),
+         le_interp = linear_interpolation(life_expectancy),
+         pop_interp = linear_interpolation(population))
+
+country_data <- left_join(country_data, metadata[, c("Country Code", "Region")], by = "Country Code")
+country_data <- country_data %>% filter(!is.na(Region))
+country_data$point_size <- sapply(country_data$pop_interp/max(country_data$pop_interp)*3000, function(x) max(x, 25))
+country_data$hover_point_size <- 2*country_data$point_size
---
+++ app.R
@@ -0,0 +1,68 @@
+source('LE_vs_FR.R')
+
+library(shiny)
+library(ggplot2)
+library(ggvis)
+
+
+regions <- c("Latin America & Caribbean", "South Asia",  "Sub-Saharan Africa", "Europe & Central Asia", "Middle East & North Africa",
+             "East Asia & Pacific", "North America")
+
+size_func <- function(data, max_size, min_size){
+  data_max <- max(data)
+  rescaled_data <- (data/data_max)*max_size
+  return(sapply(rescaled_data, function(x) max(min_size, x)))
+}
+
+ui <- fluidPage(
+  headerPanel("Fertility Rate vs Life Expectancy"),
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput("regions", label = "Region", choices = regions, selected = regions, inline = FALSE)
+    ),
+    mainPanel(
+      ggvisOutput("plot"),
+      sliderInput("year_slider", label = NULL, min = 1960, max = 2014, value = 1960, ticks = FALSE, round = TRUE,
+                  sep = "", step = 1, width = '100%',  animate=animationOptions(interval = 200)),
+      uiOutput("plot_ui")
+      ),
+    position = "right"
+  )
+)
+
+server <- function(input, output) {
+  
+  country_data_opaque <- reactive({
+    country_data %>% 
+      mutate(opacity = ifelse(Region %in% input$regions, 0.75, 0.1))
+  })
+  
+  year_data <- reactive({
+    country_data_opaque() %>% 
+      filter(year == input$year_slider)
+  })
+  
+  vis <- reactive({
+      year_data %>%
+      ggvis(x = ~le_interp, y = ~fr_interp) %>%
+      layer_points(key:= ~`Country Name`, stroke:= "black",
+                   opacity:= ~opacity, fill = ~Region,
+                   size:= ~point_size, size.hover:= ~hover_point_size) %>%
+      add_axis("x", title = "Life Expectancy") %>% 
+      add_axis("y", title = "Fertility Rate") %>% 
+      scale_numeric("y", domain = c(0.5, 9), nice = FALSE, clamp = TRUE) %>% 
+      scale_numeric("x", domain = c(10, 90), nice = FALSE, clamp = TRUE) %>% 
+      add_tooltip(function(data){
+        paste0("<b>Country:</b> ", data$`Country Name`, "<br>", 
+               "<b>Region:</b> ", data$Region, "<br>",
+               "<b>Life Expectancy:</b> ", as.character(round(data$le_interp, 2)), "<br>",
+               "<b>Fertility Rate:</b> ", as.character(round(data$fr_interp, 2)))
+      }, "hover") %>%
+      hide_legend("size") %>% 
+      # hide_legend("fill") %>% 
+      set_options(height = 600, width = 900)
+    })
+    vis %>% bind_shiny("plot", "plot_ui")
+}
+
+shinyApp(ui = ui, server = server)
